d05cfea34bdad5c808b9eb9f27d5128fded3bdcc
Wameedh <38753468+Wameedh112@users.noreply.github.com>
2018-08-10 18:54:24 +0200

    End Homework Week three Js

--- src/Contributor.js
+++ src/Contributor.js
@@ -12,11 +12,11 @@ class Contributor {
    * Render the contributor info to the DOM.
    * @param {HTMLElement} contributorList The parent element in which to render the contributor.
   */
-    // Replace this comment with your code
-    render(contributorList) { 
-      const contInfo = Util.createAndAppend('li',contributorList,{class: 'contributor'});
-      Util.createAndAppend('img', contInfo, {src:  this.data.avatar_url});
-      Util.createAndAppend('a', contInfo, {html:  this.data.login, href: contributor.html_url ,target: '_blank' });
-      Util.createAndAppend('h4', contInfo, {html:  this.data.contributions});
+  // Replace this comment with your code
+  render(contributorList) {
+    const contInfo = Util.createAndAppend('li', contributorList, { class: 'contributor' });
+    Util.createAndAppend('img', contInfo, { src: this.data.avatar_url });
+    Util.createAndAppend('a', contInfo, { html: this.data.login, href: this.html_url, target: '_blank' });
+    Util.createAndAppend('h4', contInfo, { html: this.data.contributions });
   }
 }
